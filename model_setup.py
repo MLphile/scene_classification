87e66b39572b6d2250bf62fa295644c2890f9a2e
Theophile_Ishimwe <ishimtheo@gmail.com>
2022-06-04 20:49:41 +0200

    Update model_setup.py

--- model_setup.py
+++ model_setup.py
@@ -1,13 +1,10 @@
 from torchvision import models
 import torch.nn as nn
-from data_handler import n_classes
-
 
 model = models.resnext50_32x4d(pretrained=True)
 
 inputs = model.fc.in_features
-outputs = n_classes
-
+outputs = 6
 clf = nn.Sequential(
     #               nn.Dropout(0.30),
     nn.Linear(inputs, outputs)
